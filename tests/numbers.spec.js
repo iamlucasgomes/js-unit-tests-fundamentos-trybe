932c8c29be0b40a5e77ecb9f28da4c5c069e19c5
iamlucasgomes <lucas.devjs@gmail.com>
2022-07-19 18:56:58 -0300

    UPDATE: requisito 2

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -16,10 +16,14 @@ const numbers = require('../src/numbers');
 
 describe('2 - Implemente os casos de teste para a função `numbers`', () => {
   it('Verifica se a função `numbers` retorna `true` quando o array contém apenas números e falso caso contrário', () => {
-    fail('Teste vazio!');
+    // fail('Teste vazio!');
+    expect(numbers([1, 2, 3, 4, 5])).toBeTruthy();
     // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
+    expect(numbers([1, 2, '3', 4, 5])).toBeFalsy();
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
+    expect(numbers([1, 'a', 3])).toBeFalsy();
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
+    expect(numbers([' '])).toBeFalsy();
     // Escreva um teste em que a função recebe [' '] e retorna false
   });
 });
