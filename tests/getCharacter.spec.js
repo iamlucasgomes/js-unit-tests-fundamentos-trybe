8b54bff24bcbc9ca50be2db1666702a28bd79059
iamlucasgomes <lucas.devjs@gmail.com>
2022-07-20 01:21:58 -0300

    UPDATE: requisito 9

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -43,13 +43,19 @@ Retorno:
 
 describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
   it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    fail('Teste vazio!');
+    // fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    expect(getCharacter()).toEqual(undefined);
     // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
+    expect(getCharacter('Arya')).toMatchObject({name: 'Arya Stark',class: 'Rogue',phrases: [ 'Not today', 'A girl has no name.' ]})
     // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
+    expect(getCharacter('Brienne')).toMatchObject({name: 'Brienne Tarth',class: 'Knight',phrases: [ 'Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.' ]})
     // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
+    expect(getCharacter('Melissandre')).toMatchObject({name: 'Melissandre',class: 'Necromancer',phrases: [ 'Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.' ]})
     // Teste se o parâmetro não é Case Sensitive, ou seja, independente de conter letras maiúsculas ou minúsculas retorna o mesmo objeto relativo a ele.
+    expect(getCharacter('ARYA')).toMatchObject({name: 'Arya Stark',class: 'Rogue',phrases: [ 'Not today', 'A girl has no name.' ]})
     // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
+    expect(getCharacter('Tony')).toEqual(undefined);
   });
 });
